bacb030776d23819cde5e8360649ebb604487e38
VitorMChagas <vitormchagas@gmail.com>
2021-06-27 06:48:55 -0300

    feat: Added More Colors in Theme & Global Outline

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,7 +2,11 @@ const theme = {
   colors: {
     background: '#FFF',
     text: '#151516',
-    primary: '#191847'
+    primary: '#191847',
+    secondary: '#F5850B',
+    gray: '#F4F3F6',
+    grayDetail: '#E1DEE8',
+    grayText: '#C9C5D4'
   }
 }
 
