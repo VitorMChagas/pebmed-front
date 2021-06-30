1051c0ba2fb7bb33df48a9c5a5d4b161804a98d2
VitorMChagas <vitormchagas@gmail.com>
2021-06-30 11:55:03 -0300

    fix: Correct First Page Redirection

--- next.config.js
+++ next.config.js
@@ -1,10 +1,20 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-module.exports = {
-  reactStrictMode: true,
-};
 
 const withImages = require('next-images');
 
 module.exports = withImages({
   esModule: true,
 });
+
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/checkout',
+        permanent: true,
+      },
+    ];
+  },
+  reactStrictMode: true,
+};
