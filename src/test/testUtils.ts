3a10f31ace73609ef4f74142db0e8cbac87c371a
VitorMChagas <vitormchagas@gmail.com>
2021-06-30 03:51:15 -0300

    feat: First Tests Try

---
+++ src/test/testUtils.ts
@@ -0,0 +1,23 @@
+import { render } from '@testing-library/react'
+import { ThemeProvider } from 'styled-components' 
+
+
+const Providers = ({ children }) => {
+  return children
+  // return (
+  //   <ThemeProvider theme="light">
+  //     <TranslationProvider messages={defaultStrings}>
+  //       {children}
+  //     </TranslationProvider>
+  //   </ThemeProvider>
+  // )
+}
+
+const customRender = (ui, options = {}) =>
+  render(ui, { wrapper: Providers, ...options })
+
+// re-export everything
+export * from '@testing-library/react'
+
+// override render method
+export { customRender as render }
