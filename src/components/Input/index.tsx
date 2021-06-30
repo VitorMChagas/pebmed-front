a0f10c7c420e4594f0ef10b9229b85ee7a81a297
VitorMChagas <vitormchagas@gmail.com>
2021-06-30 03:56:23 -0300

    feat: Input Validation Error Stylization

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,7 +1,6 @@
 import React, { useRef, useEffect } from 'react';
 import ReactInputMask, { Props as InputProps } from 'react-input-mask';
 import { useField } from '@unform/core';
-
 interface Props extends InputProps {
   name: string;
 }
@@ -9,6 +8,7 @@ interface Props extends InputProps {
 function InputMask({ name, ...rest }: Props) {
   const inputRef = useRef(null);
   const { fieldName, registerField, defaultValue, error } = useField(name);
+  
   useEffect(() => {
     registerField({
       name: fieldName,
@@ -22,10 +22,21 @@ function InputMask({ name, ...rest }: Props) {
       },
     });
   }, [fieldName, registerField]);
-  
+
   return (
     <div>
-      <ReactInputMask className="inputClass" ref={inputRef} defaultValue={defaultValue} {...rest} />
+        <ReactInputMask className="inputClass" ref={inputRef} defaultValue={defaultValue} {...rest} />
+        { error && 
+        <span style={{ 
+          display: 'flex', 
+          position: 'absolute', 
+          color: 'rgb(255, 0, 0)', 
+          fontSize: '0.7rem', 
+          marginTop: '-21px' 
+        }}>
+          {error}
+        </span>}
+
     </div>
   );
 };
