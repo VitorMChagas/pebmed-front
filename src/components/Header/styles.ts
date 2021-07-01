6c129cb372bec161137c54a514055662c02b8617
VitorMChagas <vitormchagas@gmail.com>
2021-06-30 22:29:45 -0300

    fix: Mobile Header Adjust

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -14,6 +14,11 @@ export const Container = styled.div`
   > img {
     width: 55px;
     height: 35px;
+
+    @media only screen and (max-width: 768px) {
+      margin-left: 60px;
+      flex-direction: row;
+    }
   }
 `;
 
